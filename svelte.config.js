89d1916d84317bf352298e0db39c7ea33183b2f3
tumrabert <tanakit.p@ayodiacompany.com>
2024-03-26 21:02:33 +0700

    little update

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/* @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
